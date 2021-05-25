a4f90f81a676fb5c03fa7c9c8cfb81bdc704c7dc
molson82 <wvandebelt82@yahoo.com>
2021-05-25 11:15:43 -0400

    :sparkles: added logic to show live pill and flash twitch icon when live

--- static/js/index.js
+++ static/js/index.js
@@ -22,10 +22,12 @@ function getTwitchStatus() {
 
 function notifyOnline() {
   document.querySelector("#dbc5f9ab-ca75-4498-8ed9-741cc4531530").classList.replace("text-black", "flash-icon");
+  document.querySelector("#twitch-live-pill").classList.replace("hidden", "block");
 }
 
 function notifyOffline() {
   document.querySelector("#dbc5f9ab-ca75-4498-8ed9-741cc4531530").classList.replace("flash-icon", "text-black");
+  document.querySelector("#twitch-live-pill").classList.replace("block", "hidden");
 }
 
 getTwitchStatus();
